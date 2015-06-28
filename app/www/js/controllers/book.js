3c9e0fd317349819527f006615908b9b3a56c92e
Sabar Dasgupta <sabar.dasgupta@gmail.com>
2015-06-27 19:02:22 -0700

    sql syntax error fix

--- app/www/js/controllers/book.js
+++ app/www/js/controllers/book.js
@@ -1,22 +1,32 @@
 angular.module('Capablanca.controllers')
 
-.controller('BooksController', function($scope, $stateParams, $ionicActionSheet, PhotosService, DataService, BooksService){
+.controller('BooksController', function($scope, $stateParams, $ionicActionSheet, $http, PhotosService, DataService, BooksService){
   BooksService.get(parseInt($stateParams.id))
   .success(function(data) {
     $scope.book = data;
     console.log($scope.book);
+
   });
+
+  function postImageData(data){
+    $http({
+      method: 'POST',
+      url: 'endpoint.com/analyze',
+      data: data,
+      headers: {'Content-Type': 'multipart/form-data'}
+    });
+  }
 
   $scope.uploadPhoto = function(){
     var processPhoto = function(data, dataType){
       if(dataType == "image"){
         $scope.user.avatar = data.toString();
         PhotosService.convertImgToBase64(data, function(imgData){
-          console.log(imgData.split(",")[1]);
+          postImageData(imgData.split(",")[1]);
         });
       }
       else if(dataType == "base64"){
-        console.log("data:image/jpeg;base64," + data);
+        postImageData(data);
       }
     }
 
